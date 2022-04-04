c48dff5170544083e1a1206b77cb66fe3cf7a704
Arturo Arreola <86033550+arturoarreola88@users.noreply.github.com>
2022-04-04 15:19:30 -0500

    did_java_functions_for_lab7

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  function alertFunction(){
+    var x1 = document.getElementById("entryinput").value;
+    var x2 = document.getElementById("textoutput");
+    alert("Arturo Arreola: "+ x1);
+    x2.innerHTML = x1;
+     }
+     var x3 = document.getElementById('entrybutton');
+     x3.addEventListener("click", alertFunction); 
+    }
 window.addEventListener('load', init);
